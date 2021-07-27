b9bbeb6fb4f6ba9a176d5ea7e6f49a887b3c6ac1
webcoderz <19884161+webcoderz@users.noreply.github.com>
2021-07-27 11:44:35 -0500

    downgrade react maps gl

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,39 @@
 module.exports = {
-    webpack: (config) => {
-        config.module.rules.push({
-            resolve:{
-                alias: {
-                    ...config.resolve.alias,
-                    'mapbox-gl': 'maplibre-gl'
-                }
+  mode: 'development',
+
+  entry: {
+    app: './map.js'
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        // Compile ES2015 using babel
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env', '@babel/react']
             }
-        })
-      // Important: return the modified config
-      return config
-    },
-  }
+          }
+        ]
+      }
+    ]
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+        resolve:{
+            alias: {
+                ...config.resolve.alias,
+                'mapbox-gl': 'maplibre-gl'
+            }
+        }
+    })
+  // Important: return the modified config
+  return config
+},
+}
