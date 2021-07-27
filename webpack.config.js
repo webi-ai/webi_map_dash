3beef4bd00f1b0139b04437bd0bb45e7fa3207b0
webcoderz <19884161+webcoderz@users.noreply.github.com>
2021-07-27 11:26:28 -0500

    webconfig alias to maplibre instead of react maps (which isnt rendering)

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,14 @@
 module.exports = {
-  mode: 'development',
-
-  entry: {
-    app: './index.js'
-  },
-
-  devtool: 'source-map',
-
-  module: {
-    rules: [
-      {
-        // Compile ES2015 using babel
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/env', '@babel/react']
+    webpack: (config) => {
+        config.module.rules.push({
+            resolve:{
+                alias: {
+                    ...config.resolve.alias,
+                    'mapbox-gl': 'maplibre-gl'
+                }
             }
-          }
-        ]
-      }
-    ]
+        })
+      // Important: return the modified config
+      return config
+    },
   }
