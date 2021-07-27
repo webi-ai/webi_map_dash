77ff2900822d7a0bcda0b1c02a3ed0781244ca0f
webcoderz <19884161+webcoderz@users.noreply.github.com>
2021-07-26 21:49:01 -0500

    add favicon

---
+++ src/map.js
@@ -0,0 +1,102 @@
+
+import {StaticMap, MapContext, NavigationControl,GeolocateControl} from 'react-map-gl';
+import DeckGL, {GeoJsonLayer, ArcLayer} from 'deck.gl';
+import React, {useState,useRef, useCallback} from 'react';
+import Geocoder from 'react-map-gl-geocoder';
+import MapGL from "react-map-gl";
+import Directions from './directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
+
+// Set your mapbox access token here
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoid2ViY29kZXJ6IiwiYSI6ImNrcjZ1N3oxeDB0cHoyd3FsYjk0am9kY3MifQ.lw9n5DtqV-PjMyL4k6jwQA';
+
+
+
+const NAV_CONTROL_STYLE = {
+  position: 'fixed',
+  bottom: 30,
+  right :10
+};
+
+
+function GeoLocate() {
+  const geolocateControlStyle= {
+    position: 'fixed',
+    right: 10,
+    bottom: 118
+  };
+  const {map} = React.useContext(MapContext)
+  return (
+    
+    <GeolocateControl
+      style={geolocateControlStyle}
+      positionOptions={{enableHighAccuracy: true}}
+      trackUserLocation={true}
+      auto = {false}
+    />
+  );
+}
+
+
+
+function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
+
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides
+      });
+    },
+    [handleViewportChange]
+  );
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        width="100%"
+        height="100%"
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+      > 
+        <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          position = "top-left"
+        />
+      <NavigationControl style={NAV_CONTROL_STYLE} />
+     <GeoLocate/>
+     <Directions mapRef={mapRef} mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} overflow-x = "scroll"/>
+      </MapGL>
+    </div>
+  );
+};
+
+
+
+
+
+
+
+export default Map
+
+
+
+
